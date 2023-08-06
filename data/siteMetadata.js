da9f44dfbe1da51e2911c8e64273178f37746450
Shahzad Afridi <shahzadahmadafridi@gmail.com>
2023-08-06 17:26:06 +0500

    Push changes

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,9 +1,9 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  title: 'Realtime Coding',
+  author: 'Shahzad Afridi',
+  headerTitle: 'Realtime Coding',
+  description: 'Sharing is caring and a better way to learn new things then to transform that knowledge to learners.',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
