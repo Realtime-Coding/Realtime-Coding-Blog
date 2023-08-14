f4c368e5059f0fbd42398441d3069db7f4e9243b
Shahzad Afridi <shahzadahmadafridi@gmail.com>
2023-08-15 04:23:01 +0500

    Fixed changes

--- layouts/ListLayoutWithTags.tsx
+++ layouts/ListLayoutWithTags.tsx
@@ -124,7 +124,7 @@ export default function ListLayoutWithTags({
           <div className="hidden max-h-screen h-full sm:flex flex-wrap bg-gray-50 dark:bg-gray-900/70 shadow-md pt-5 dark:shadow-gray-800/40 rounded min-w-[280px] max-w-[280px]">
             <div className="py-4 px-6">
               {pathname.startsWith('/blog') ? (
-                <h3 className="text-primary-500 font-bold uppercase">All Posts</h3>
+                <h3 className="font-bold uppercase" style={{ color: '#e75d60' }}>All Posts</h3>
               ) : (
                 <Link
                   href={`/blog`}
