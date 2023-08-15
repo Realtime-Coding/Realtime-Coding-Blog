6132d6b2dee8cab09f00a16a1935ce5ab159a678
Shahzad Afridi <shahzadahmadafridi@gmail.com>
2023-08-15 22:36:29 +0500

    Init commit

---
+++ data/headerNavLinks.ts
@@ -0,0 +1,10 @@
+const headerNavLinks = [
+  { href: '/', title: 'Home' },
+  { href: '/blog', title: 'Blog' },
+  { href: '/tags', title: 'Tags' },
+  { href: '/projects', title: 'Projects' },
+  { href: '/youtube', title: 'Youtube' },
+  { href: '/about', title: 'About' },
+]
+
+export default headerNavLinks
